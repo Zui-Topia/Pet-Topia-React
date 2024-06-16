1719b95429127f2a1ee85782df133179b54b47e3
EunchanJeong <jec65147@gmail.com>
2024-06-16 21:18:17 +0900

    [#17]feat : 예약 페이지 js 파일 생성 및 라우터, App.js에 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { ROUTES } from './constants/routes';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Map from './pages/Map/map';
 import MyPage from './pages/MyPage/mypage';
+import Reservation from './pages/Reservation/reservation';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path={ROUTES.MAP} element={<Map />}></Route>
                 <Route path={ROUTES.MYPAGE} element={<MyPage />}></Route>
+                <Route path={ROUTES.RESERVATION} element={<Reservation />}></Route>
             </Routes>
         </BrowserRouter>
     );
