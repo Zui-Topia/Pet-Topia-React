885f1d5bbd4593858d15222504be96e9690aa4dc
EunchanJeong <jec65147@gmail.com>
2024-06-16 21:34:47 +0900

    [#19]feat : 예약 페이지 '지점 검색' 상단에 위치시키기

--- src/pages/Reservation/reservation.js
+++ src/pages/Reservation/reservation.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Title from '../../components/Main/Title/Title';
+import Title from './../../components/Main/Title/Title';
+import BranchSearch from './../../components/Map/BranchSearch/BranchSearch';
 
 const Reservation = () => {
     return (
         <div>
             <Title />
+            <BranchSearch />
         </div>
     );
 };
