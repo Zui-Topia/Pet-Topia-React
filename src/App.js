ae769f22829efcc090c72bdd6b19f782c00e217e
JAESEONGIM <bigstar2878@gmail.com>
2024-06-16 21:09:25 +0900

    feat : 회원가입 페이지 생성
    {추가} :
    Components : User/Input/Input.js + Password.js추가 ( 마이페이지, 로그인, 회원가입 에서 사용 할 입력상자 컴포넌트 추가 )
    Input.js : 텍스트 입력상자
    Password.js : 비밀번호 입력상자 ( ***처리)
    Route.js : 회원가입 경로 추가
    pages/SignUP/signup.js 회원가입 페이지 추가
    
    {수정} :Title.js - 폰트, 크기, 굵기 수정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import { ROUTES } from './constants/routes';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Map from './pages/Map/map';
-import MyPage from './pages/MyPage/mypage';
+import { ROUTES } from "./constants/routes";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Map from "./pages/Map/map";
+import MyPage from "./pages/MyPage/mypage";
+import Signup from "./pages/SignUp/singup";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={ROUTES.MAP} element={<Map />}></Route>
-                <Route path={ROUTES.MYPAGE} element={<MyPage />}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.MAP} element={<Map />}></Route>
+        <Route path={ROUTES.MYPAGE} element={<MyPage />}></Route>
+        <Route path={ROUTES.SIGNUP} element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
