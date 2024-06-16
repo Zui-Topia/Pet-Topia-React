8e8c087f6942bcd972deff3bcb74ee7dae47c128
yookyung Choi <ykc131719@gmail.com>
2024-06-16 15:52:20 +0900

    feat : 마이페이지 js 파일 생성 및 라우터, App.js에 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { ROUTES } from './constants/routes';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Map from './pages/Map/map';
+import MyPage from './pages/MyPage/mypage';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={ROUTES.MAP} element={<Map />}></Route>
+                <Route path={ROUTES.MYPAGE} element={<MyPage />}></Route>
             </Routes>
         </BrowserRouter>
     );
