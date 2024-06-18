c6000fcab8911cc77dd899c2b809a769352135ea
Do Yeon <mju1025@naver.com>
2024-06-18 11:54:37 +0900

    feat : 지도에 마커 표시

---
+++ src/components/Map/CategoryButton/CategoryMarker.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+// 마커 버튼 - 아이콘
+const CategoryMarker = styled.img`
+    width: 38px;
+    height: 39px;
+    position: absolute;
+`;
+
+export default CategoryMarker;
