476b7146a33be4642b99f592d62b4e68aaa0314b
Do Yeon <mju1025@naver.com>
2024-06-19 22:06:31 +0900

    feat : 상세 정보 제공 모달띄우기

--- src/components/Map/CategoryButton/MarkerRenderer.js
+++ src/components/Map/CategoryButton/MarkerRenderer.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MARKER } from '../../../constants/imagePaths';
 import CategoryMarker from './CategoryMarker';
+import { Popover } from 'antd';
 
-// 이미지와 좌표 정보를 받는 타입 정의
 const MarkerRenderer = ({ markerData }) => {
+    const [visible, setVisible] = useState({});
+
+    const handleVisibleChange = (key, visible) => {
+        setVisible({ ...visible, [key]: visible });
+    };
+
     return (
         <div>
             {markerData.map((data, idx) => (
-                <CategoryMarker
+                <Popover
                     key={idx}
-                    src={MARKER[data.categoryId]}
-                    style={{ left: `${data.placePositionX}px`, top: `${data.placePositionY}px` }}
-                    alt={`Image ${data.categoryId}`}
-                />
+                    title="위치"
+                    content={`${data.placeInfo}`}
+                    visible={visible[idx] || false}
+                    placement="top"
+                    onVisibleChange={(newVisible) => handleVisibleChange(idx, newVisible)}
+                >
+                    <CategoryMarker
+                        src={MARKER[data.categoryId]}
+                        style={{ left: `${data.placePositionX}px`, top: `${data.placePositionY}px` }}
+                        alt={`Image ${data.categoryId}`}
+                        onClick={() => handleVisibleChange(idx, true)}
+                    />
+                </Popover>
             ))}
         </div>
     );
