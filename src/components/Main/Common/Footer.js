03ecb0ef38f7ad13bef420d16ecf0165b0a45480
JAESEONGIM <bigstar2878@gmail.com>
2024-06-19 15:52:57 +0900

    design : footer 크기 동적 확대/축소 적용

--- src/components/Main/Common/Footer.js
+++ src/components/Main/Common/Footer.js
@@ -6,6 +6,10 @@ const FooterContainer = styled.div`
   width: 100vw;
   height: 200px;
   position: absolute;
+
+  background-size: cover; /* 이미지를 커버하도록 설정 */
+  background-position: center; /* 이미지를 가운데 정렬 */
+
   bottom: 0;
   background-image: url(${MAIN_IMAGES_PATHS.MAIN_FOOT});
 `;
