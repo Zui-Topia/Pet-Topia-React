946448d0ac14cbd071df7c7676ffefaf7643bf21
yookyung Choi <ykc131719@gmail.com>
2024-06-19 11:06:25 +0900

    feat : mypage 나의 예약 통신

--- src/pages/MyPage/mypage.js
+++ src/pages/MyPage/mypage.js
@@ -1,5 +1,5 @@
-// pages/mypage.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import SectionTitle from '../../components/MyPage/SectionTitle';
 import MyPageSection from '../../components/MyPage/MyPageSection';
 import UserInfo from '../../components/MyPage/UserInfo';
@@ -7,8 +7,27 @@ import ReservationInfo from '../../components/MyPage/ReservationInfo';
 import ReservationHead from '../../components/MyPage/ReservationHead';
 import ReservationBody from '../../components/MyPage/ReservationBody';
 import QRModal from '../../components/MyPage/QRModal';
+import MyReservationAPI from '../../api/MyPage/MyPageAPI';
 
 const MyPage = () => {
+    const [reservations, setReservations] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchReservations = async () => {
+            try {
+                const response = await MyReservationAPI(2);
+                setReservations(response.data);
+                console.log(response.data);
+            } catch (error) {
+                setError(error);
+                console.error(error);
+            }
+        };
+
+        fetchReservations();
+    }, []);
+
     return (
         <>
             <div></div>
